6e95312435780eb28c270f367a8583ed4497deae
ASRomdon <dablegcoblan@gmail.com>
2020-08-29 07:02:40 +0700

    fix(*): adding fontawesome 4

--- app/app.js
+++ app/app.js
@@ -3,8 +3,9 @@
 // Declare app level module which depends on views, and core components
 angular.module('myApp', [
   'ngRoute',
-  'myApp.view1',
+  'listService',
   'myApp.view2',
+  'Edit',
   'myApp.version'
 ]).
   config(['$locationProvider', '$routeProvider', function ($locationProvider, $routeProvider) {
